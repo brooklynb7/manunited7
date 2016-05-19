7ba8433f3f1a112f893b3b13dae240f7ea536c82
brooklynb7 <brooklynb7@gmail.com>
2016-05-19 13:23:34 +0800

    Update the ui for phone

--- public/javascripts/index/post.js
+++ public/javascripts/index/post.js
@@ -14,7 +14,7 @@
 		UI.BlockUI.show();
 		Service.getPostBySlug(slug)
 			.done(function(rst) {
-				$(selector.post).html(createPostContent(rst));
+				$(selector.post).html(UI.Post.createPost(rst));
 			})
 			.fail(function(jqXHR) {
 				console.log(jqXHR);
@@ -22,27 +22,6 @@
 			.always(function() {
 				UI.BlockUI.hide();
 			});
-	}
-
-	function createPostContent(post) {
-		var $div = $('<div class="post-block panel panel-default"/>');
-
-		var $panelBody = $('<div class="panel-body" />');
-		var $title = $('<h3>' + post.title + '</h3>');
-		var $time = $('<h6>' + UI.convertDateTime(post.create_at) + '</h6>');
-		var $jia = $('<div class="jiathis_style_24x24" style="margin-bottom:10px;" />');
-		_.each(['tsina', 'weixin', 'douban', 'fb', 'twitter'], function(item) {
-			$jia.append('<a class="jiathis_button_' + item + '" />');
-		});
-		$jia.append(
-			'<script type="text/javascript" src="http://v3.jiathis.com/code_mini/jia.js?uid=1344332343487662" charset="utf-8"></script>'
-		).append('<div class="clear" />');
-		var $content = $('<div class="post-content">' + post.content + '</div>');
-		$panelBody.append($title).append($time).append($jia).append($content);
-
-		$div.append($panelBody);
-
-		return $div;
 	}
 
 }(jQuery, window.slug));
---
+++ public/javascripts/util/post.ui.js
@@ -0,0 +1,72 @@
+'use strict';
+
+(function($) {
+	var createPanel = function() {
+		return $('<div class="row post-block panel panel-default"/>');
+	};
+
+	var createPanelBody = function() {
+		return $('<div class="panel-body" />');
+	};
+
+	var createTitle = function(title) {
+		return $('<div class="title">' + title + '</h3>');
+	};
+
+	var createTitleLink = function(title, link) {
+		return $('<div class="title"><a href="' + link + '">' + title + '</a></div>');
+	};
+
+	var createTime = function(time) {
+		return $('<h6 class="time">' + time + '</h6>');
+	};
+
+	var createContent = function(content) {
+		return $('<div class="post-content">' + content + '</div>');
+	};
+
+	var createJia = function() {
+		var $jia = $('<div class="jiathis_style_24x24" style="margin-bottom:10px;" />');
+		_.each(['tsina', 'weixin', 'douban', 'fb', 'twitter'], function(item) {
+			$jia.append('<a class="jiathis_button_' + item + '" />');
+		});
+		$jia.append(
+			'<script type="text/javascript" src="http://v3.jiathis.com/code_mini/jia.js?uid=1344332343487662" charset="utf-8"></script>'
+		).append('<div class="clear" />');
+
+		return $jia;
+	};
+
+	var createPostListItem = function(post) {
+		var $div = createPanel();
+		var $panelBody = createPanelBody();
+		var $title = createTitleLink(post.title, '/posts/' + post.slug);
+		var $time = createTime(UI.convertDateTime(post.create_at));
+		var $content = createContent(post.content);
+		$panelBody.append($title).append($time).append($content);
+		$div.append($panelBody);
+
+		return $div;
+	};
+
+	var createPost = function(post) {
+		var $div = createPanel();
+		var $panelBody = createPanelBody();
+		var $title = createTitle(post.title);
+		var $time = createTime(UI.convertDateTime(post.create_at));
+		var $jia = createJia();
+		var $content = createContent(post.content);
+		$panelBody.append($title).append($time).append($jia).append($content);
+
+		$div.append($panelBody);
+
+		return $div;
+	};
+
+	window.UI = window.UI || {};
+
+	window.UI.Post = {
+		createPost: createPost,
+		createPostListItem: createPostListItem
+	};
+}(jQuery));
